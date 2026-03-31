71bfcb00e1c4eac17fca522d186bb30532669172
shashwathv <shashwthv4405@gmail.com>
2026-03-31 09:03:13 +0530

    models

--- src/audio_model/api/models/schemas.py
+++ src/audio_model/api/models/schemas.py
@@ -14,6 +14,6 @@ class StatusResponse(BaseModel):
     listening: bool
     latest: Optional[DetectionEvent] = None
 
-class HistroyRepsonse(BaseModel):
+class HistoryRepsonse(BaseModel):
     total: int
     events: list[DetectionEvent]
