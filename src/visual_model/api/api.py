5e48c4038ea6d43a411802baf646e89aeca567b4
shashwathv <shashwthv4405@gmail.com>
2026-04-04 01:06:38 +0530

    deleted

--- src/visual_model/api/api.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-from api.routes import predict, health
-
-router = APIRouter()
-router.include_router(health.router, tags=["Health"])
-router.include_router(predict.router, tags=["Prediction"])
